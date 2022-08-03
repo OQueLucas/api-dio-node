a056fc26c9ab63b6dbb3884dc05850f1bd2e74e5
OQueLucas <lucasestevam365@gmail.com>
2022-08-03 10:53:23 -0300

    create the lasts tests to user controller

--- src/controllers/CreateUserController.test.ts
+++ src/controllers/CreateUserController.test.ts
@@ -1,14 +1,26 @@
+import { getConnection } from 'typeorm';
 import createConnection from '../database';
 import { CreateUserController } from './CreateUserController';
 import { Request } from 'express';
 import { mockResponse } from '../utils/mocks/mockResponse';
 
 describe('CreateUserController', () => {
-  it('Deve retornar status 201 quando o usuario for criado', async () => {
+  beforeAll(async () => {
     const connection = await createConnection();
     await connection.runMigrations();
-    const createUserController = new CreateUserController();
+  });
 
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.query('DELETE FROM usuarios');
+    await connection.close();
+  });
+
+  const createUserController = new CreateUserController();
+
+  const response = mockResponse();
+
+  it('Deve retornar status 201 quando o usuario for criado', async () => {
     const request = {
       body: {
         nome: 'Algum usuario',
@@ -16,11 +28,33 @@ describe('CreateUserController', () => {
       },
     } as Request;
 
-    const response = mockResponse();
+    await createUserController.handle(request, response);
+
+    expect(response.state.status).toBe(201);
+  });
+
+  it('Deve retornar status 400 quando o nome não for informado', async () => {
+    const request = {
+      body: {
+        nome: '',
+        email: 'email@email.com',
+      },
+    } as Request;
 
     await createUserController.handle(request, response);
 
-    await connection.query('DELETE FROM usuarios');
+    expect(response.state.status).toBe(400);
+  });
+
+  it('Deve retornar status 201 quando o email não for informado', async () => {
+    const request = {
+      body: {
+        nome: 'Algum usuario',
+        email: '',
+      },
+    } as Request;
+
+    await createUserController.handle(request, response);
 
     expect(response.state.status).toBe(201);
   });
